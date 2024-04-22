2060a971e1bd187d5e0e98a3a0d1a6d8c5ebcb17
Emranhossain <emran.shuvo.dev@gmail.com>
2024-04-22 21:18:52 +0600

    worked on left banner js file changed the Ui Designer to code debugger

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -5,7 +5,7 @@ import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
+      words: ["Professional Coder.", "Mern Stack Developer.", "Code Debugger."],
       loop: true,
       typeSpeed: 20,
       deleteSpeed: 10,
@@ -16,7 +16,7 @@ const LeftBanner = () => {
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">John doe</span>
+          Hi, I'm <span className="text-designColor capitalize">Md. Emran Hossain</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
