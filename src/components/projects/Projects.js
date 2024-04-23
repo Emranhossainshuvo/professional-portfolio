7c663d5c82b270713256f5507fd2bd2e3a62d131
Emranhossain <emran.shuvo.dev@gmail.com>
2024-04-23 14:22:25 +0600

    added one project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -11,16 +11,15 @@ const Projects = () => {
     >
       <div className="flex justify-center items-center text-center">
         <Title
-          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
           des="My Projects"
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
+          title="Online restaurant"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
+          src={"https://i.ibb.co/DrKWqzv/2150671588.jpg"}
         />
         <ProjectsCard
           title="E-commerce Website"
