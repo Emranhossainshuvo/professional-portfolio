8317d468b6faa6e63ba768e9d3cdea87bcf197a6
Emranhossain <emran.shuvo.dev@gmail.com>
2024-04-23 14:02:47 +0600

    added Mern stack as a new feature of this portfolio

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe } from "react-icons/fa";
+import { FaGlobe, FaAddressCard, FaServer, FaUserLock } from "react-icons/fa";
 import { SiProgress, SiAntdesign } from "react-icons/si";
 import Title from '../layouts/Title';
 import Card from './Card';
@@ -16,23 +15,22 @@ const Features = () => {
         <Card
           title="Frontend"
           des="As a frontend developer, I love creating dynaic and functional web applications also I love to add animation to my work."
-         
+          icon={<FaAddressCard />}
         />
         <Card
           title="Backend"
-          des="I love working with backend because it's more techiee than the frontend although I'm not much good at it but I love working with it"
-          icon={<AiFillAppstore />}
+          des="I love working with backend because it's more techiee than the frontend although I'm not much good at it but I love working with it."
+          icon={<FaServer />}
         />
         <Card
           title="MERN stack"
-          des=""
+          des="Besically I am a MERN stact developer I use MongoDB Express React Node etc for my development."
           icon={<SiProgress />}
         />
         <Card
-          title="Mobile Development"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
-          icon={<FaMobile />}
+          title="Authentication"
+          des=""
+          icon={<FaUserLock />}
         />
         <Card
           title="UX Design"
